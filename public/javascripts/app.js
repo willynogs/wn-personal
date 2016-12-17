5bea1065259627a64853c793a729425c9246b30a
willynogs <willynogs@gmail.com>
2016-12-17 18:02:07 -0500

    mobile menu update

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,9 +1,12 @@
+//angular
 var app = angular.module('wnPersonal', [])
 
 .controller('MainCtrl', ['$scope', function($scope){
 
 }]);
 
+//jquery
 $('.burger').click(function(){
   $(this).toggleClass('burger-clicked');
+  $('.wrapper--outer-mobile').toggleClass('mobile--menu--show');
 });
