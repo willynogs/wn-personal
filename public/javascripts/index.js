41998e9edfab58ac83f320df6f58c30a0273d48a
willynogs <willynogs@gmail.com>
2016-12-19 09:50:59 -0500

    add movement

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,13 +1,14 @@
 var circle = new mojs.Shape({
   shape:        'circle',
-  radius:       20,
+  radius:       {0 : 20},
   fill:         'none',
   stroke:       '#fff',
   strokeWidth:  1,
   x: -60,
   y: -10,
+  duration: 1000,
   isShowStart:  true
-});
+}).play();
 
 var tri = new mojs.Shape({
   shape:        'polygon',
@@ -18,19 +19,23 @@ var tri = new mojs.Shape({
   strokeWidth: 1,
   x: -70,
   y: 15,
+  angle: {330 : 360},
+  duration: 800,
+  delay: 200,
   isShowStart:  true
-});
+}).play();
 
 var zigzag = new mojs.Shape({
   shape:        'zigzag',
-  points:       17,
-  radius:       55,
+  points:       {0 : 17},
+  radius:       {0 : 55},
   radiusY:      10,
   fill:         'none',
   stroke:       '#fff',
   strokeWidth: 1,
   angle: 0,
-  x: 0,
+  x: {22.5 : 0},
   y: 45,
+  duration: 1000,
   isShowStart:   true,
-});
+}).play();
