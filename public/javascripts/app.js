ee780add18dbdbe730a6af69284da2de740d23a2
willynogs <willynogs@gmail.com>
2016-12-20 08:27:51 -0500

    email support

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,18 @@
 //angular
 var app = angular.module('wnPersonal', [])
 
-.controller('MainCtrl', ['$scope', function($scope){
+.controller('MainCtrl', ['$scope', '$http', function($scope, $http){
   $scope.name = '';
   $scope.email = '';
   $scope.msg = '';
 
   $scope.submit = function(){
-    $scope.message = 'sending your message...';
+    //$scope.message = 'sending your message...';
+    $http.get(`/submit-contact/${$scope.name}/${$scope.email}/${$scope.msg}`)
+      .then(function(r){
+        //r.status == '200' ? $scope.message = 'message sent!' : $scope.message = 'error! please retry!';
+        console.log(r.status);
+      });
   }
 }]);
 
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var nodemailer = require('nodemailer');
 var router = express.Router();
 
 /* GET home page. */
@@ -13,8 +14,26 @@ router.get('/about', function(req, res, next){
 router.get('/work', function(req, res, next){
   res.render('work', { title: 'Work'});
 });
+
 router.get('/contact', function(req, res, next){
   res.render('contact', { title: 'Contact'});
+});
+
+router.get('/submit-contact/:name/:email/:msg', function(req, res, next){
+  var transporter = nodemailer.createTransport('smtps://willynogs%40gmail.com:spencer513@smtp.gmail.com');
+  var mailOptions = {
+    from: `"${req.params.name}" <${req.params.email}>`,
+    to: 'willynogs@gmail.com',
+    subject: '🔥 new msg 🔥',
+    text: `From: ${req.params.name} <${req.params.email}> \n Message: ${req.params.msg}`
+  };
+  transporter.sendMail(mailOptions, function(error, info){
+    if(error){
+        return console.log(error);
+    }
+    console.log('Message sent: ' + info.response);
+  });
+  res.send(req.data);
 });
 
 module.exports = router;
